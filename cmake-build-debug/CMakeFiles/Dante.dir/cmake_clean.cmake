061fbbcff7de08c2905bd8eadfe218d42b9c73b6
Maciusssiek <64867638+Maciusssiek@users.noreply.github.com>
2021-01-27 16:36:30 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Dante.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dante.dir/main.c.obj"
+  "Dante.exe"
+  "Dante.exe.manifest"
+  "Dante.pdb"
+  "libDante.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Dante.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
