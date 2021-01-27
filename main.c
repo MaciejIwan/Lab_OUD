718b66bd11a303ad029cfede821b791083912845
Maciusssiek <64867638+Maciusssiek@users.noreply.github.com>
2021-01-27 17:18:28 +0100

    Test

--- main.c
+++ main.c
@@ -14,12 +14,8 @@ struct Point{
 int main() {
     struct Point pts[10]; // deklaracja nowej tablicy
     struct Line line = {2.5, 7};
+    printf("Mam na imie Maciej");
     //
-    for (int i = 0; i < 10; ++i) {
-        pts[i].x = i;
-        pts[i].y = line.a * i + line.b;
-        printf("%f %f \n", pts[i].x, pts[i].y);
-    }
 
     return 0;
 }
